623273d8255a45e25cd88f9a3b223e8f29f55cc9
Fabricio Fuentes <fuentesfabricio17@gmail.com>
2025-11-24 17:17:25 -0300

    urls

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -9,7 +9,7 @@ import './App.css';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/frontend">
         <App />
       </BrowserRouter>
     </AuthProvider>
